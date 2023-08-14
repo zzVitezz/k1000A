a8cfad39760e08c72bf2a2631dc9fb8630bb8d6d
llVitell <omar.vite.a@uni.pe>
2023-08-14 11:29:13 -0500

    Animacion de entrada y salida para el modalMenu

--- src/components/OvModalMenu.vue
+++ src/components/OvModalMenu.vue
@@ -8,11 +8,12 @@
   <Teleport to="body">
     <div
       v-if="modal"
-      class="fixed z-40 inset-0 bg-black bg-opacity-30 backdrop-blur"
+      class="fixed z-40 inset-0 bg-black bg-opacity-30 backdrop-blur aea"
     >
       <div
         ref="modalRef"
-        class="fixed left-full lg:w-[25%] w-full h-full z-40 translate-x-[-100%] bg-white aea"
+        class="fixed left-full lg:w-[25%] w-full h-full z-40 translate-x-[-100%] bg-white"
+        :class="animation"
       >
         <section class="menu">
           <button @click="closeModal" class="flex justify-end">
@@ -38,30 +39,43 @@
   </Teleport>
 </template>
 <script setup lang="ts">
-import { onClickOutside } from "@vueuse/core";
-import { ref } from "vue";
+import { onClickOutside } from "@vueuse/core"
+import { ref } from "vue"
 
-const modal = ref(false);
-const modalRef = ref(null);
+const modalRef = ref(null) 
+const modal = ref(false)
+const animation = ref('slide-left')
+const isAnimating = ref(false)
 
 onClickOutside(modalRef, (event) => {
-  modal.value = false;
+  modal.value = false
+  animation.value = 'slide-left'
 });
 
 const closeModal = () => {
-  modal.value = false;
+  animation.value = 'slide-right'
+  if (!isAnimating.value) {
+    isAnimating.value = true
+    setTimeout(() => {
+      modal.value = false
+      animation.value = 'slide-left'
+      isAnimating.value = false
+    }, 200)
+  }
+
 };
 </script>
 <style scoped>
 .menu{
   @apply w-full h-full p-5 flex flex-col justify-evenly;
 }
-
-.aea{
-  animation:slide-left 0.15s linear both
-}@keyframes slide-left{0%{transform:translateX(0)}200%{transform:translateX(-100px)}}
 .menu-item {
   @apply text-xl lg:hover:underline lg:decoration-amber-600 lg:decoration-2 lg:hover:underline-offset-4;
 }
+.slide-left{animation:slide-left 0.1s linear both}@keyframes slide-left{0%{transform:translateX(0)}100%{transform:translateX(-100%)}}
+
+.slide-right{animation:slide-right 0.1s linear both}@keyframes slide-right{0%{transform:translateX(-100%)}100%{transform:translateX(100%)}}
+
+
 
 </style>
